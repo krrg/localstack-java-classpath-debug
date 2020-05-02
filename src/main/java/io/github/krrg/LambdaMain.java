dc90d6acb290e1be6afc5fc832b811502bf91a8c
krrg <krrgithub@gmail.com>
2020-05-01 23:28:22 -0600

    Initial commit

---
+++ src/main/java/io/github/krrg/LambdaMain.java
@@ -0,0 +1,31 @@
+package io.github.krrg;
+
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Map;
+
+public class LambdaMain implements RequestHandler<Map<String, String>, String> {
+
+    @Override
+    public String handleRequest(Map<String, String> input, Context context) {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder
+                .standard()
+                .withEndpointConfiguration(
+                        new AwsClientBuilder.EndpointConfiguration(
+                                "http://localhost:4566",
+                                "us-east-1"
+                        )
+                )
+                .build();
+
+        client.listTables().getTableNames().forEach(tableName -> {
+            System.out.println("Found Dynamo table: " + tableName);
+        });
+
+        return "";
+    }
+}
